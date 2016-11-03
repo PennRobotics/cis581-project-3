6f05cb599ac2dd9385b4247e53411ecaacf0a6ee
Brian Wright <brwr@mit.edu>
2016-11-03 15:58:29 -0400

    Initial code (unmodified template)

---
+++ anms.m
@@ -0,0 +1,9 @@
+% Adaptive Non-Maximal Suppression
+% cimg = corner strength map
+% max_pts = number of corners desired
+% [x, y] = coordinates of corners
+% rmax = suppression radius used to get max_pts corners
+
+function [x, y, rmax] = anms(cimg, max_pts)
+    
+end
---
+++ feat_desc.m
@@ -0,0 +1,10 @@
+% img = double (height)x(width) array (grayscale image) with values in the
+% range 0-255
+% x = nx1 vector representing the column coordinates of corners
+% y = nx1 vector representing the row coordinates of corners
+% descs = 64xn matrix of double values with column i being the 64 dimensional
+% descriptor computed at location (xi, yi) in im
+
+function [descs] = feat_desc(img, x, y)
+
+end
