bd511e8ed8e5027a3e75e71895f2215dcf7a7948
Brian Wright <brianwright@mail.usf.edu>
2016-11-06 17:00:03 -0500

    Run this script

--- project3.m
+++ project3.m
@@ -1,1 +1,22 @@
-% TODO(brwr)
+%% Automatic 2D Image Mosaic
+% CIS581 Computer Vision, University of Pennsylvania
+% Author: Brian Wright
+
+clear all; clc
+
+%% Import images
+imageFiles = dir('images');
+numImages = length(imageFiles) - 2;
+for i = 3 : length(imageFiles)
+  imagePath = ['images/' imageFiles(i).name];
+  images{i - 2} = imread(imagePath);
+  if (DEBUG) figure(i); imagesc(images{i - 2}); end
+end
+
+%% Stitch images
+imageComposite = mymosaic(images);  % TODO(brwr): Ensure uint8, 3-ch cell array input
+
+%% Output
+figure(1)
+imagesc(imageComposite)
+% TODO(brwr): Save image
